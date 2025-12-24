7a9bfef74f5e50d6bc9cdfd9a16159afa9f70ee3
Jay Hevelone <kehevelone@eurac.org>
2025-12-24 10:21:20 +0100

    Solutions Day 1

--- AdventOfCode.Solutions/Day01/Day01Solver.cs
+++ AdventOfCode.Solutions/Day01/Day01Solver.cs
@@ -3,28 +3,71 @@ namespace AdventOfCode.Solutions.Day01;
 public class Day01Tests
 {
     private readonly Day01Solver _solver = new();
-    [Test] public async Task Step1WithExample() => await _solver.ExecuteExample1("??");
-    [Test] public async Task Step2WithExample() => await _solver.ExecuteExample2("??");
+    [Test] public async Task Step1WithExample() => await _solver.ExecuteExample1("11");
+    [Test] public async Task Step2WithExample() => await _solver.ExecuteExample2("31");
     [Test] public void Step1WithPuzzleInput() => TestContext.Current?.OutputWriter.WriteLine(_solver.ExecutePuzzle1());
     [Test] public void Step2WithPuzzleInput() => TestContext.Current?.OutputWriter.WriteLine(_solver.ExecutePuzzle2());
 }
 
 public class Day01Solver : SolverBase
 {
-    // List<??> _data;
+    List<(int Left, int Right)> _data = new();
 
     protected override void Parse(List<string> data)
     {
-        // _data = new();
+        _data = data.Select(line =>
+        {
+            var sides = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            return (Left: int.Parse(sides[0]), Right: int.Parse(sides[1]));
+        }).ToList();
     }
 
     protected override object Solve1()
     {
-        throw new Exception("Solver error");
+        var (leftListNumbers, rightListNumbers) = SplitAndOrderData(_data);
+        int sum = 0;
+
+        for (int i = 0; i < leftListNumbers.Count; i++)
+        {
+            sum += Math.Abs(leftListNumbers[i] - rightListNumbers[i]);
+        }
+
+        return sum;
     }
 
     protected override object Solve2()
     {
-        throw new Exception("Solver error");
+        var (leftListNumbers, rightListNumbers) = SplitAndOrderData(_data);
+        Dictionary<int, int> rightListCounts = new Dictionary<int, int>();
+        int sum = 0;
+        
+        for (int i = 0; i < rightListNumbers.Count; i++)
+        {
+            if (rightListCounts.ContainsKey(rightListNumbers[i]))
+            {
+                rightListCounts[rightListNumbers[i]] += 1;
+            }
+            else
+            {
+                rightListCounts[rightListNumbers[i]] = 1;
+            }
+        }
+
+        foreach (int number in leftListNumbers)
+        {
+            if (rightListCounts.ContainsKey(number))
+            {
+                sum += number * rightListCounts[number];
+            }
+        }
+
+        return sum;
+    }
+
+    private (List<int> left, List<int> right) SplitAndOrderData(List<(int Left, int Right)> _data)
+    {
+        var leftListNumbers = _data.Select(p => p.Left).OrderBy(x => x).ToList();
+        var rightListNumbers = _data.Select(p => p.Right).OrderBy(x => x).ToList();
+        return (leftListNumbers, rightListNumbers);
     }
 }
